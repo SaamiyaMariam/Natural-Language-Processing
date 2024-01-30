ceeb0ea76f448b809701daa2d2495fc4b78e4b9f
Saamiya <125141049+SaamiyaMariam@users.noreply.github.com>
2024-01-30 22:12:23 +0500

    Update spider.py

--- Web Scraping with Scrapy for Urdu Stories Curation/spider.py
+++ Web Scraping with Scrapy for Urdu Stories Curation/spider.py
@@ -1,26 +1,26 @@
-import scrapy
+# Saamiya Mariam 20i-0612
+# NLP Assignment 02
+import scrapy, re
+# re library for regular expressions
 
-class UrduStoriesSpider(scrapy.Spider):
-    name = 'I200612urdu_stories_spider'
-    start_urls = ['https://www.urduzone.net']
+class I200612UrduStoriesSpiderSpider(scrapy.Spider):
+    name = "i200612_urdu_stories_spider"
+    allowed_domains = ["urduzone.net"]
+    start_urls = ["https://urduzone.net"]
 
     def parse(self, response):
-        # Extract story links from the main page
-        story_links = response.xpath('//a[@rel="bookmark"]/@href').extract()
-        for link in story_links:
-            yield response.follow(link, self.parse_story)
 
-    def parse_story(self, response):
-        # Extract the story title and text
-        title = response.xpath('//title/text()').get()
-        story_text = response.xpath('//p[@dir="rtl"]/text()').getall()
-        story_text = ' '.join(story_text)  # Joining if the text is in multiple parts
+        # removing HTML tags and extracting text data
+        text_data = response.css(' ').extract_first()
 
-        # Optional: Clean the title to remove extra text
-        title = title.split('|')[0].strip() if title else title
+        # since we want to keep urdu text only, we will filter out non-urdu words
+        urdu_text = re.findall(r'[\u0600-\u06FF\s]+', text_data)
+        processed_urdu = " ".join(urdu_text)
 
-        yield {
-            'title': title,
-            'story': story_text
+        # used to output data from the spider. 
+        # stores it in an internal buffer and yields it to the Scrapy framework to be processed and saved
+        yield 
+        {
+            'urdu_text': processed_urdu
         }
-
+        pass
